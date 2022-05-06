64a6e84287f8d204b480f4bdbffe4b1923eb79c4
jialiang <jialiangrao@gmail.com>
2022-05-06 17:18:17 +0900

    添加注释

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-// config environment variables
+// 配置环境变量
 require('dotenv').config({ path: './configs.env' });
 const mongoose = require('mongoose');
 const app = require('./app');
 
-// get connect url
+// 处理连接到mongodb的url
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
@@ -11,7 +11,7 @@ const DB = process.env.DATABASE.replace(
 
 const port = process.env.PORT || 3000;
 
-// connect mongoose to database
+// 用mongoose中间件连接mongodb数据库
 mongoose.connect(DB).then(() => console.log('DB connectted ✌'));
 
 app.listen(port, () =>
