2f33d74d44ac2c0566400e32564859048ebb5989
jialiang <jialiangrao@gmail.com>
2022-05-06 17:12:43 +0900

    添加 tour-stats 的路由逻辑

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -95,15 +95,33 @@ exports.deleteTour = async (req, res) => {
   }
 };
 
-// exports.getTourStats = async (req, res) => {
-//   try {
-//     const stats = Tour.aggregate([
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: null,
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+    ]);
 
-//     ]);
-//   } catch (err) {
-//     res.status(404).json({
-//       status: 'fail',
-//       message: err.message,
-//     });
-//   }
-// };
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+};
